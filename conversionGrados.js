116a7c8265d402d1fa03a74439e714851f383272
Caleb <caleb.aves@gmail.com>
2023-05-27 22:26:19 -0300

    Agrego el archivo conversionGrados.js

---
+++ conversionGrados.js
@@ -0,0 +1,7 @@
+let fahrenheit;
+fahrenheit = 50;
+let celcius = (fahrenheit - 32) * 5/9;
+console.log(fahrenheit, "grados fahrenheit es igual a", celcius, "grados celcius.");
+fahrenheit = 75;
+celcius = (fahrenheit - 32) * 5/9;
+console.log(fahrenheit, "grados fahrenheit es igual a", celcius, "grados celcius.");

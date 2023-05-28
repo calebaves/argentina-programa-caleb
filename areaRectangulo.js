a1c0db3e6e8217f21fab6cb4311faf42959138c0
Caleb <caleb.aves@gmail.com>
2023-05-27 22:27:51 -0300

    Practico N0 finalizado

---
+++ areaRectangulo.js
@@ -0,0 +1,8 @@
+let base;
+let altura;
+base = 5;
+altura = 3;
+console.log("El area de un rectangulo de base", base, "y de altura", altura, "es igual a:", base * altura);
+base = 12.5;
+altura = 5.3;
+console.log("El area de un rectangulo de base", base, "y de altura", altura, "es igual a:", base * altura);
---
+++ promedio3.js
@@ -0,0 +1,15 @@
+let numero1;
+let numero2;
+let numero3;
+numero1 = 5;
+numero2 = 7;
+numero3 = 9;
+console.log("El promedio entre", numero1,",", numero2, "y", numero3, "es", (numero1 + numero2 + numero3) / 3);
+numero1 = -2;
+numero2 = -3;
+numero3 = -8;
+console.log("El promedio entre", numero1,",", numero2, "y", numero3, "es", (numero1 + numero2 + numero3) / 3);
+numero1 = -1.2;
+numero2 = 5.3;
+numero3 = -3.3;
+console.log("El promedio entre", numero1,",", numero2, "y", numero3, "es", (numero1 + numero2 + numero3) / 3);
